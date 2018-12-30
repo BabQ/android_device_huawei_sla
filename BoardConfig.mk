ab1cc8b2ebad24651bf13144be7a4e2d992b4485
babq <1153791778@qq.com>
2018-12-30 14:53:02 +0800

    modify size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,13 +34,13 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
 
 # Encryption
-TARGET_CRYPTFS_HW_PATH := $(LOCAL_PATH)/cryptfs_hw
+#TARGET_CRYPTFS_HW_PATH := $(LOCAL_PATH)/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Keymaster
